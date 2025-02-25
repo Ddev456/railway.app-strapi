7b48a5bba2d4fe2c6e19c69794f81e9f0ff31ea0
Dylan DELHALLE <74488220+Ddev456@users.noreply.github.com>
2025-02-25 07:18:21 +0100

    fix ipv6

--- config/server.ts
+++ config/server.ts
@@ -1,15 +1,18 @@
 export default ({ env }) => ({
-  host: 'localhost',
-  port: 1337,
-  url: env('STRAPI_URL', 'http://localhost:1337'),
-  app: {
-      keys: env.array('APP_KEYS'),
-  },
-  security: {
-      cors: {
-          enabled: true,
-          origin: [env('FRONTEND_URL', 'http://localhost:3000')],
-          credentials: true,
-      },
-  },
+    host: env('NODE_ENV') === 'production' ? '::' : 'localhost',
+    port: env.int('PORT', 1337),
+    url: env('URL', 'http://localhost:1337'),
+    app: {
+        keys: env.array('APP_KEYS'),
+    },
+    security: {
+        cors: {
+            enabled: true,
+            origin: [
+                env('FRONTEND_URL', 'http://localhost:3000'),
+            ],
+            credentials: true,
+        },
+    },
+    proxy: env('NODE_ENV') === 'production'
 });
